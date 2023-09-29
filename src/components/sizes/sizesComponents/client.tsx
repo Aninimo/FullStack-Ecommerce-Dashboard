321dd1d7fbf40f8a8975e256a98311910bea1a81
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-09-29 13:37:29 -0300

    Create client.tsx

---
+++ src/components/sizes/sizesComponents/client.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router'
+import { Plus } from 'lucide-react'
+
+import { ApiList } from '../../ui/api-list'
+import { Heading } from '../../ui/heading'
+import { Button } from '../../ui/button'
+import { DataTable } from '../../ui/data-table'
+import { Separator } from '../../ui/separator'
+import { columns, SizeColumn } from './columns'
+
+interface SizesClientProps {
+  data: SizeColumn[];
+}
+
+export const SizesClient: React.FC<SizesClientProps> = ({
+  data
+}) => {
+  const router = useRouter()
+  const params = router.query
+
+  return (
+    <>
+      <div className='flex items-center justify-between'>
+        <Heading title={`Sizes (${data.length})`} description='Manage sizes for your products'/>
+        <Button onClick={() => router.push(`/${params.storeId}/routes/sizes/new`)}>
+          <Plus className='mr-2 h-4 w-4'/> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey='name' columns={columns} data={data} />
+      <Heading title='API' description='API Calls for Sizes'/>
+      <Separator />
+      <ApiList entityName='sizes' entityIdName='sizeId'/>
+    </>
+  );
+};
