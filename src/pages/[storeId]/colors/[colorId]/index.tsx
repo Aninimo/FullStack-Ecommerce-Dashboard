5fbbd2e8579bf360deddd4604e554f0e1acc30c4
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-09-29 13:16:54 -0300

    Create index.tsx

---
+++ src/pages/[storeId]/colors/[colorId]/index.tsx
@@ -0,0 +1,52 @@
+import { GetServerSidePropsContext } from 'next'; 
+
+import { ColorForm } from '../../../../../components/colors/colorIdComponents/color-form'
+import prismadb from '../../../../../lib/prismadb'
+import { ColorProps } from '../../../../../types'
+
+interface ColorPageProps {
+  color: ColorProps | null;
+}
+
+export default function ColorPage({ color }: ColorPageProps){
+  return(
+    <div className='flex-col'>
+      <div className='flex-1 space-y-4 p-8 pt-6'>
+        <ColorForm initialData={color} />
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const colorId = context.params?.colorId as string | undefined; 
+
+  try {
+    const color = await prismadb.color.findUnique({
+      where: {
+        id: colorId
+      }
+    });
+
+    if (!color) {
+      return {
+        props: {
+          color: null,
+        }
+      };
+    }
+
+    return {
+      props: {
+        color: JSON.parse(JSON.stringify(color)),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        color: null,
+      }
+    };
+  }
+}
