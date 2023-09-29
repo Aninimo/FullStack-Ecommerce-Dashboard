b4cc96536957a604a70de3539ab6c708a09e0597
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-09-29 13:15:02 -0300

    Create index.tsx

---
+++ src/pages/[storeId]/categories/[categoryId]/index.tsx
@@ -0,0 +1,76 @@
+import { GetServerSidePropsContext } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+import { CategoryForm } from '../../../../../categories/categoryIdComponents/components/category-form';
+import prismadb from '../../../../../lib/prismadb';
+import { BillboardProps, CategoryProps } from '../../../../../types'
+
+const prisma = new PrismaClient()
+
+interface CategoryPageProps {
+  category: CategoryProps | null;
+  billboards: BillboardProps[] | null;
+}
+
+export default function CategoryPage({ 
+  category, 
+  billboards
+}: CategoryPageProps) {
+  if (billboards === null) {
+    return <div>Loading...</div>; 
+  }
+  
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <CategoryForm billboards={billboards} initialData={category} />
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) { 
+  const storeId = context.params?.storeId as string;
+  const categoryId = context.params?.categoryId as string;
+
+  try {
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: storeId,
+      },
+    });
+
+    if (!categoryId || !/^[0-9a-fA-F]{24}$/.test(categoryId)) {
+      return {
+        props: {
+          billboards: JSON.parse(JSON.stringify(billboards)),
+          category: null,
+        },
+      };
+    }
+
+    const category = await prisma.category.findUnique({
+      where: {
+        id: categoryId,
+      },
+    });
+
+    return {
+      props: {
+        billboards: JSON.parse(JSON.stringify(billboards)),
+        category: JSON.parse(JSON.stringify(category)),
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+
+    return {
+      props: {
+        billboards: null,
+        category: null,
+      },
+    };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
