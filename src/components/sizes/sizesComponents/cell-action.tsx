6381e713da09eb6ef2c38709152107a4e73093dd
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-09-29 13:36:19 -0300

    Create cell-action.tsx

---
+++ src/components/sizes/sizesComponents/cell-action.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react'
+import { toast } from 'react-hot-toast'
+
+import { AlertModal } from '../../modals/alert-modal'
+import { Button } from '../../ui/button'
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuLabel, 
+  DropdownMenuTrigger
+} from '../../ui/dropdown-menu'
+import { SizeColumn } from './columns'
+
+interface CellActionProps {
+  data: SizeColumn;
+}
+
+export function CellAction({
+  data
+}: CellActionProps){
+  const router = useRouter()
+  const params = router.query
+  
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch(`/api/${params.storeId}/sizes/${data.id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      if (response.ok) {
+        toast.success('Size deleted.')
+        window.location.reload()
+      } else {
+        toast.error('Failed to delete size. Make sure you removed all products using this size first.')
+      }
+    } catch (error) {
+      console.error(error)
+      toast.error('An error occurred while deleting the size.')
+    } finally {
+      setOpen(false)
+      setLoading(false)
+    }
+  }
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id)
+    toast.success('Size ID copied to clipboard.')
+  }
+
+  const onUpdate = () => {
+    router.push(`/${params.storeId}/routes/sizes/${data.id}`)
+  }
+
+  return (
+    <>
+      <AlertModal 
+        isOpen={open} 
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='h-8 w-8 p-0'>
+            <span className='sr-only'>Open menu</span>
+            <MoreHorizontal className='h-4 w-4'/>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className='mr-2 h-4 w-4'/> Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={onUpdate}>
+            <Edit className="mr-2 h-4 w-4" /> Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className='mr-2 h-4 w-4' /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
