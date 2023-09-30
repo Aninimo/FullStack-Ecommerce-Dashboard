7b42860d56034a18f1ff41fa706187abf5535dc9
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-09-30 17:20:54 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,9 @@
-import { authMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/:path*", "/favicon.ico"],
+});
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
-};
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
