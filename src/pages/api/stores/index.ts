b83282b74c536433ddb9f70db7741130ff2d1039
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-01 17:15:29 -0300

    Update index.ts

--- src/pages/api/stores/index.ts
+++ src/pages/api/stores/index.ts
@@ -1,37 +1,46 @@
-import { withServerSideAuth } from '@clerk/nextjs/ssr';
-
+import { NextApiRequest, NextApiResponse } from 'next'
+import { auth } from '@clerk/nextjs';
+ 
 import prismadb from '../../../lib/prismadb'
 
-const handler = withServerSideAuth(async (req, res) => {
-  const { userId } = req.auth;
+export default async function handler(
+  req: NextApiRequest, 
+  res: NextApiResponse
+) {
   try {
-    if (req.method === 'POST') {
-      const { userId } = getAuth(req);
-      const { body } = req;
+    if(req.method === 'POST'){
+      const { userId } = auth()
+      const { body } = req
     
-      const { name } = body;
-    
+      const { name } = body
+
       if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-    
+        return res.status(401).json({ error: 'Unauthorized' })
+    }
+
       if (!name) {
-        return res.status(400).json({ error: 'Name is required' });
+        return res.status(400).json({ error: 'Name is required' })
       }
-    
+
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' })
+      }
+
+      if (!name) {
+        return res.status(400).json({ error: 'Name is required' })
+      }
+
       const store = await prismadb.store.create({
         data: {
           name,
           userId,
-        },
-      });
-    
-      return res.status(200).json(store);
+        }
+      })
+  
+      return res.status(200).json(store)
     }
   } catch (error) {
-    console.error('[STORES_POST]', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    console.error('[STORES_POST]', error)
+    return res.status(500).json({ error: 'Internal Server Error' })
   }
-}, {});
-
-export default handler;
+}
