627662da24aa49fa2a3e2534af9e47c9e3a51214
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-01 20:35:22 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useAuth } from '@clerk/nextjs/server';
-
+import { useAuth } from "@clerk/nextjs";
+ 
 import SetupPage from './SetupPage';
 import prismadb from '../lib/prismadb';
 
 export default function MyPage() {
   const router = useRouter();
-  const auth = useAuth();
-  const userId = auth.userId || '';
+  const { userId } = useAuth();
   const [hasStore, setHasStore] = useState(null);
 
   useEffect(() => {
