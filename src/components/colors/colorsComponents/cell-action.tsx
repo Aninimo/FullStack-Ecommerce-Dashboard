b1ef061bc56ad517b7eba35611e01a10661fa6cc
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-01 09:21:17 -0300

    Update cell-action.tsx

--- src/components/colors/colorsComponents/cell-action.tsx
+++ src/components/colors/colorsComponents/cell-action.tsx
@@ -80,7 +80,7 @@ export function CellAction({
             <Copy className='mr-2 h-4 w-4'/> Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/${params.storeId}/routes/colors/${data.id}`)}
+            onClick={() => router.push(`/${params.storeId}/colors/${data.id}`)}
           >
             <Edit className='mr-2 h-4 w-4'/> Update
           </DropdownMenuItem>
