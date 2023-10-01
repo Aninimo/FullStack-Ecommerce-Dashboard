0338beb17404274581a0c2b8369f0c7e1c7ef133
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-01 13:53:53 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { withServerSideAuth } from '@clerk/nextjs/ssr'
-import { getAuth } from '@clerk/nextjs/server'
 
 import SetupPage from './SetupPage'
 import prismadb from '../lib/prismadb'
@@ -13,7 +12,7 @@ export default function App() {
 }
 
 export const getServerSideProps = withServerSideAuth(async ({ req, res }) => {
-  const { userId } = getAuth(req);
+  const { userId } = req.auth;
 
   if (!userId) {
     res.writeHead(302, { Location: '/sign-in' });
