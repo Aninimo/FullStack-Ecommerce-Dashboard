b2c2041d80e0f65da442aa0e99bff2071ada1fef
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-01 17:05:18 -0300

    Update index.ts

--- src/pages/api/stores/index.ts
+++ src/pages/api/stores/index.ts
@@ -5,41 +5,33 @@ import prismadb from '../../../lib/prismadb'
 const handler = withServerSideAuth(async (req, res) => {
   const { userId } = req.auth;
   try {
-    if(req.method === 'POST'){
-      const { userId } = getAuth(req)
-      const { body } = req
+    if (req.method === 'POST') {
+      const { userId } = getAuth(req);
+      const { body } = req;
     
-      const { name } = body
-
+      const { name } = body;
+    
       if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' })
-    }
-
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    
       if (!name) {
-        return res.status(400).json({ error: 'Name is required' })
+        return res.status(400).json({ error: 'Name is required' });
       }
-
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' })
-      }
-
-      if (!name) {
-        return res.status(400).json({ error: 'Name is required' })
-      }
-
+    
       const store = await prismadb.store.create({
         data: {
           name,
           userId,
-        }
-      })
-  
-      return res.status(200).json(store)
+        },
+      });
+    
+      return res.status(200).json(store);
     }
   } catch (error) {
-    console.error('[STORES_POST]', error)
-    return res.status(500).json({ error: 'Internal Server Error' })
+    console.error('[STORES_POST]', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+}, {});
 
-export default handler
+export default handler;
