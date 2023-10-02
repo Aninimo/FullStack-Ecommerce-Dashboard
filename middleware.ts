e194f1eb3110c4d2412bc5db4d92ee2d621de8ee
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-02 17:30:29 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
+import { authMiddleware } from '@clerk/nextjs';
+
 export default authMiddleware({
-  publicRoutes: ["/api/webhook"]
+  publicRoutes: ["/api/:path*"],
 });
- 
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
