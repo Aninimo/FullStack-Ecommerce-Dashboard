179ad8d9318adc93a92b08ba07d4f9df2794d25b
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-03 18:54:29 -0300

    Update client.tsx

--- src/components/products/productsComponents/client.tsx
+++ src/components/products/productsComponents/client.tsx
@@ -23,7 +23,7 @@ export function ProductsClient({
     <> 
       <div className='flex items-center justify-between'>
         <Heading title={`Products (${data.length})`} description='Manage products for your store' />
-        <Button onClick={() => router.push(`/${params.storeId}/routes/products/new`)}>
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
           <Plus className='mr-2 h-4 w-4'/> Add New
         </Button>
       </div>
