b6704086fdac23d2da0681eb355353d200fc0b2a
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-03 12:55:33 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
-import { authMiddleware } from '@clerk/nextjs';
-
-export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { withClerkMiddleware } from '@clerk/nextjs/server'
+ 
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
 });
-
+ 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+}
