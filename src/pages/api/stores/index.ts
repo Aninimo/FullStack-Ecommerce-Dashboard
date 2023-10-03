25ccf7ea0371dfe8097e9f702a92ea691a30ccb2
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-03 12:23:32 -0300

    Update index.ts

--- src/pages/api/stores/index.ts
+++ src/pages/api/stores/index.ts
@@ -1,15 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getAuth } from '@clerk/nextjs/server';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { requireAuth, RequireAuthProp } from '@clerk/nextjs/api'
  
 import prismadb from '../../../lib/prismadb'
 
-export default async function handler(
-  req: NextApiRequest, 
+export default requireAuth(async (
+  req: RequireAuthProp<NextApiRequest>, 
   res: NextApiResponse
-) {
+) => {
   try {
     if(req.method === 'POST'){
-      const { userId } = getAuth(req)
+      const { userId } = req.auth
       const { body } = req
     
       const { name } = body
@@ -17,14 +17,6 @@ export default async function handler(
       if (!userId) {
         return res.status(401).json({ error: 'Unauthorized' })
     }
-
-      if (!name) {
-        return res.status(400).json({ error: 'Name is required' })
-      }
-
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' })
-      }
 
       if (!name) {
         return res.status(400).json({ error: 'Name is required' })
