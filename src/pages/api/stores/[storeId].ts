4e67282b8c306ca0dd71a744e2b5fc37c3fe0b2e
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-10-03 14:11:54 -0300

    Update [storeId].ts

--- src/pages/api/stores/[storeId].ts
+++ src/pages/api/stores/[storeId].ts
@@ -5,7 +5,7 @@ import prismadb from '../../../lib/prismadb'
 
 export default requireAuth(async (
   req: RequireAuthProp<NextApiRequest>,
-  res: NextApiResponse
+  res: NextApiResponse<void>
 )  => {
   try {
     if (req.method === 'PATCH') {
